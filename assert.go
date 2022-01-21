0250f4189009401d8805f4b20f73513ba2ed5c66
Liam Egan <liam@liamegan.com>
2022-01-20 20:57:13 -0500

    Reverse order of actual and expected parameters

--- assert.go
+++ assert.go
@@ -13,7 +13,7 @@ func New(test *testing.T) Assert {
 	return Assert{test: test}
 }
 
-func (assert Assert) Equal(actual, expected interface{}) {
+func (assert Assert) Equal(expected, actual interface{}) {
 	if !reflect.DeepEqual(actual, expected) {
 		assert.test.Errorf("expected: %#+v; got: %#+v", expected, actual)
 	}
