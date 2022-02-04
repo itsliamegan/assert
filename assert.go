a964fe732bac0e670d7ab579272a39a4e37a2b56
Liam Egan <liam@liamegan.com>
2022-02-03 20:53:59 -0500

    Add calls to T.Helper

--- assert.go
+++ assert.go
@@ -14,18 +14,24 @@ func New(test *testing.T) Assert {
 }
 
 func (assert Assert) Ok(value interface{}) {
+	assert.test.Helper()
+
 	if value == nil {
 		assert.test.Fatal("expected non-nil")
 	}
 }
 
 func (assert Assert) NoErr(err error) {
+	assert.test.Helper()
+
 	if err != nil {
 		assert.test.Fatalf("expected no error, got %#+v", err)
 	}
 }
 
 func (assert Assert) Equal(actual, expected interface{}) {
+	assert.test.Helper()
+
 	if !reflect.DeepEqual(actual, expected) {
 		assert.test.Fatalf("expected %#+v, got %#+v", expected, actual)
 	}
